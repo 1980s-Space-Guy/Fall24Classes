73a8f53fb5fd687a4c1e94e1a960df36f92a1f5c
William Roosa <wroosa2@gmail.com>
2024-10-09 15:53:23 -0500

    xjbvdsjbv

--- NumericalAnalysisCode/Chap7/EulerExplicit.m
+++ NumericalAnalysisCode/Chap7/EulerExplicit.m
@@ -1,19 +1,39 @@
-f1=@(x,y1,y2) y1-2*y2;
-f2=@(x,y1,y2) 2*y1-4*y2;
+f=@(x,y) x*cos(x)+y;
+fprime=@(x,y) cos(x)-x*sin(x)+f(x,y);
 
 x(1)=0;
-y1(1)=0;
-y2(1)=1;
+y(1)=0.5;
+
+error(1)=0;
 
 n=1;
-xf=0.5;
+xf=0.05;
 step=(xf-x(1))/n;
 
 for i=1:n
     x(i+1)=x(i)+step; %#ok<SAGROW>
-    y1(i+1)=y1(i)+step*f1(x(i),y1(i),y2(i)); %#ok<SAGROW>
-    y2(i+1)=y2(i)+step*f2(x(i),y1(i),y2(i)); %#ok<SAGROW>
+    y(i+1)=y(i)+step*f(x(i),y(i)); %#ok<SAGROW>
+    error(i+1)=fprime(x(i), y(i))*step^2/2; %#ok<SAGROW>
 end
 
-disp(y1)
-disp(y2)
+disp(y)
+fprintf('%1.6f\n', error(2))
+%% Explicit Euler with error control (What the built in function does)
+f=@(x,y) x*cos(x)+y;
+fprime=@(x,y) cos(x)-x*sin(x)+f(x,y);
+
+x(1)=0;
+y(1)=0.5;
+
+TOL=1e-6;
+
+for i=1:10000
+    if x(i) > 1
+        break
+    end
+    dx=sqrt(TOL*2/abs(fprime(x(i),y(i))));
+    x(i+1)=x(i)+dx; %#ok<SAGROW>
+    y(i+1)=y(i)+dx*f(x(i),y(i)); %#ok<SAGROW>
+end
+
+disp(y)
--- NumericalAnalysisCode/Chap7/EulerImplicit.m
+++ NumericalAnalysisCode/Chap7/EulerImplicit.m
@@ -7,7 +7,7 @@ xf=0.05;
 
 for i=x:step:xf-step
     fz=@(yp1) -yp1+y+step*f(i+step,yp1);
-    yp1=fzero(fz,y, optimset('Display', 'off'));
+    yp1=fzero(fz, y, optimset('Display', 'off'));
     y=yp1;
 end
 
