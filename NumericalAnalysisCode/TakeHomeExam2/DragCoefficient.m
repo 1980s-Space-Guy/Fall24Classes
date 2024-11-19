695752a461fddee2c50d30c0bd637797f7b12fe3
William Roosa <wroosa2@gmail.com>
2024-11-19 15:18:14 -0600

    fseiodng

---
+++ NumericalAnalysisCode/TakeHomeExam2/DragCoefficient.m
@@ -0,0 +1,16 @@
+function Cd = DragCoefficient(Re,v)
+    if Re<10e-4
+        Cd=240000;
+    elseif Re<0.2
+        Cd=24/Re;
+    elseif Re<1000
+        Cd=24/Re*(1+0.14*Re^0.7);
+    elseif Re<2000
+        Cd=21.12/Re+6.3/sqrt(Re)+0.25;
+    else
+        Ma=v/343;
+        Cd0=21.12/Re+6.3/sqrt(Re)+0.25;
+        Cd=Cd0*((1-0.445*Ma+4.84^Ma^2-9.73*Ma^3+6.93*Ma^4/sqrt(1+1.2*Ma*Cd0)));
+    end
+end
+
---
+++ NumericalAnalysisCode/TakeHomeExam2/PartC.m
@@ -0,0 +1,62 @@
+function PartC
+    % Creating Spline functions from the data table given
+    altitude=Altitude();
+    gravity_func=spline(altitude.height,altitude.gravity);
+    density_func=spline(altitude.height,altitude.density);
+    viscosity_func=spline(altitude.height,altitude.viscosity);
+    
+    % Ball info
+    r=0.05; % m
+    rho_ball=2500; % kg/m3
+    m=4/3*pi*r^3*rho_ball; % kg
+    drop_height=2000; %m
+
+    % Solving ODE
+    tspan=[0,300]; % Simulating the ball being dropped from t=0 s to t=75 s
+    y0=[drop_height,0]; % Dropping the ball from rest at 1000 m, so the 
+                        % speed at t = 0 s is 0 m/s
+    options=odeset('RelTol',10e-9,'AbsTol',10e-9);
+    function ode=FallingODE(t,z)
+        % z(1) is y, z(2) is y'
+        % Interpolating for gravity, density and viscosity at the altitude
+        g=ppval(gravity_func,z(1));
+        rho_air=ppval(density_func,z(1));
+        mu=ppval(viscosity_func,z(1));
+        % Taking the absolute value of the velocity was needed for
+        % finding the drag coefficient
+        Re=rho_air*abs(z(2))*2*r/mu;
+        Cd=DragCoefficient(Re,abs(z(2)));
+        % Ode function
+        ode(1,1)=z(2);
+        ode(2,1)=-g+1/2*rho_air*Cd*pi*r^2*z(2)^2/m;
+    end
+    ode_soln=ode45(@FallingODE,tspan,y0,options);
+
+    % Plotting resulting y function
+    tt=linspace(0,300,1000);
+    yy=deval(ode_soln,tt,1);
+    plot(tt,yy);
+
+    % Using nonlinear solver to find the time when the ball hits the ground
+    guess=[250,300];
+    function height=height_func(time)
+        height=deval(ode_soln,time,1);
+    end
+    options=optimset('Display','off');
+    ground_hit_time=fzero(@height_func,guess,options);
+    % Getting the speed when the ball hits the ground
+    ground_hit_speed=abs(deval(ode_soln,ground_hit_time,2));
+
+    % Calculating the change in thermal energy
+    grav_int=fnint(gravity_func);
+    change_PE=m*(ppval(grav_int,drop_height)-ppval(grav_int,0));
+    change_KE=1/2*m*ground_hit_speed^2;
+    change_TE=-change_KE-change_PE;
+    TE_generated=abs(change_TE);
+
+    % Output
+    fprintf('Time taken to hit ground: %2.3f s\n',ground_hit_time)
+    fprintf('Speed when ground hit: %2.3f m/s\n',ground_hit_speed)
+    fprintf('Thermal energy generated: %5.0f J\n\n', TE_generated)
+    fprintf('Energy lost from friction shouls be the same as the thermal energy generated')
+end
